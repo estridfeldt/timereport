f90aa70eca3503d86e9b3eb7e2871fd7be2b548b
emst <emil.stridfeldt@gmail.com>
2019-08-19 10:12:50 +0200

    Rename main app.

--- src/main/java/se/emst/timereport/TimeReportApplication.java
+++ src/main/java/se/emst/timereport/TimeReportApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TimereportApplication {
+public class TimeReportApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TimereportApplication.class, args);
+        SpringApplication.run(TimeReportApplication.class, args);
     }
 
 }
