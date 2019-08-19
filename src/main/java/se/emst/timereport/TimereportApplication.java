58875eb5d70700ecf265d07e63f39ed62c592eff
emst <emil.stridfeldt@gmail.com>
2019-08-19 09:28:13 +0200

    Project skeleton

---
+++ src/main/java/se/emst/timereport/TimereportApplication.java
@@ -0,0 +1,13 @@
+package se.emst.timereport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimereportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimereportApplication.class, args);
+    }
+
+}
